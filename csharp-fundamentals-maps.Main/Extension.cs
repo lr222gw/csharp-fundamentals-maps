cefef7264ea8aab27fc1e20669829aa3cd5c318e
Lowe Raivio <Lowe.Raivio@se.experis.com>
2025-01-08 12:14:41 +0100

    Core and Extensions completed

--- csharp-fundamentals-maps.Main/Extension.cs
+++ csharp-fundamentals-maps.Main/Extension.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace csharp_fundamentals_maps.Main
 {
-    public  class Extension
+    public class Extension
     {
         private Dictionary<string, float> _planets;
 
@@ -16,7 +16,7 @@ namespace csharp_fundamentals_maps.Main
             _planets = new Dictionary<string, float>();
             _planets.Add("Jupiter", 5.2f);
             _planets.Add("Uranus", 19.2f);
-            _planets.Add("Pluto", 39f);
+            //_planets.Add("Pluto", 39f);
             _planets.Add("Mercury", 0.39f);
             _planets.Add("Saturn", 9.54f);
             _planets.Add("Earth", 1f);
@@ -26,17 +26,20 @@ namespace csharp_fundamentals_maps.Main
         }
         //TODO   Pluto is unfortunately no longer a planet so please comment out the add line!
 
-        
-        public Dictionary<string,int> LettersInName()
+
+        public Dictionary<string, int> LettersInName()
         {
-           
+
             Dictionary<string, int> result = new Dictionary<string, int>();
 
             //TODO   Complete this method to return an Dictionary of <string,int> which contains 
             //          the planet name and the number of letters in its name
             //          iterate the _planets using a foreach object to load the result dictionary.
 
-            
+            foreach (var p in this._planets.Keys)
+            {
+                result[p] = p.Length;
+            }
 
             return result;
         }
@@ -46,13 +49,14 @@ namespace csharp_fundamentals_maps.Main
 
 
 
-        public Dictionary<string,float> OrderedPlanets()
-        {           
-                return _planets.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);                           
+        public Dictionary<string, float> OrderedPlanets()
+        {
+            return _planets.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
         }
         public Dictionary<string, float> OrderedPlanetsByDescending()
-        {            
-            return _planets.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        {
+            //return _planets.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            return _planets.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
         }
         //TODO:  modify the OrderedPlanetsByDescending so it is not dictionary is not doing an OrderBy but OrderByDescending
 
@@ -63,11 +67,12 @@ namespace csharp_fundamentals_maps.Main
         //       furthest from the sun.
         //       Update the method to return the correct KeyValuePair's Key (the string)!
         //       Use the ClosestToTheSun as a guide
-        
+
 
         public string FurthestFromTheSun()
         {
-            return string.Empty;      
+            KeyValuePair<string, float> result = OrderedPlanets().Last();
+            return result.Key;
         }
         public string ClosestToTheSun()
         {
@@ -76,7 +81,7 @@ namespace csharp_fundamentals_maps.Main
             return result.Key;
         }
 
-        public Dictionary<string,float> Planets { get { return _planets; } }
+        public Dictionary<string, float> Planets { get { return _planets; } }
 
 
     }
